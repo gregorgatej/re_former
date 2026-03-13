78ca90d68ad08be4bf361c5d1672420f61163485
Gregor Gatej <gregor.gatej@gmail.com>
2026-03-13 05:57:46 +0100

    feat: update routes and controller to handle editing an existing user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,22 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_path
+      redirect_to root_path, notice: "User #{@user.username} created successfully."
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to root_path, notice: "User updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
